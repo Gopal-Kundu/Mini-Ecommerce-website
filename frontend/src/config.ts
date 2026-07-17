4539482cd29dc496767e019c3ff3c6c32f8ba91f
Gopal Kundu <kundugopal2005@gmail.com>
2026-07-17 07:11:24 +0530

    Type changed from js to ts

---
+++ frontend/src/config.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://localhost:5000/api';
+export const API_URL = `${BASE_URL}/auth`;
+export const API_PRODUCTS = `${BASE_URL}/products`;
+export const API_CART = `${BASE_URL}/cart`;
