bb449ee494a0f150b79cde330330bab10d92f7e9
Gopal Kundu <kundugopal2005@gmail.com>
2026-07-17 07:21:42 +0530

    Backend url added

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:5000/api';
+// export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = 'https://mini-ecommerce-website-lrgz.vercel.app/api';
 export const API_URL = `${BASE_URL}/auth`;
 export const API_PRODUCTS = `${BASE_URL}/products`;
 export const API_CART = `${BASE_URL}/cart`;
